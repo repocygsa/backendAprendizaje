b7e697adf9ada242eb598e9b3dcf6160e54a3928
RAV-DV <randradeva@jej.cl>
2024-03-19 17:42:59 -0300

    genImgLargo

--- src/puppeteer/generarImgStatus.js
+++ src/puppeteer/generarImgStatus.js
@@ -7,7 +7,7 @@ const generarImagesStatus = async({url})=>{
       headless:'new',
       defaultViewport:{
           width:1366,
-          height:2800,
+          height:3800,
           deviceScaleFactor:1,
           isMobile:true,
           hasTouch:false,
